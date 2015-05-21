3b3cb290aa54a4f12f295f438ba808c9e6cceb33
DrewDahlman <info@drewdahlman.com>
2015-05-20 20:47:51 -0600

    cleanup

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
 // Base App
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
 
 // Routes
-var routes = require('./routes/index');
-var api = require('./routes/api');
+var routes = require('./routes/index'),
+    api = require('./routes/api');
 
 // Database
-var mongo = require('mongodb');
-var monk = require('monk');
+var mongo = require('mongodb'),
+    monk = require('monk');
 
 // Start our Express App
 var app = express();
 
-// var db = monk('legbot:legw0rk!@ds031792.mongolab.com:31792/heroku_app37057289');
-console.log(process.env.NODE_ENV, process.env.MONGO_USER, process.env.MONGO_PW);
-
 if(process.env.NODE_ENV == "development"){
-  var db = monk('localhost:27017/leglights');
+  var mongo_db = 'localhost:27017/leglights';
 } else {
-  console.log(process.env.MONGO_USER+':'+process.env.MONGO_PW+'@ds031792.mongolab.com:31792/heroku_app37057289');
-  
-  var db = monk(process.env.MONGO_USER+':'+process.env.MONGO_PW+'@ds031792.mongolab.com:31792/heroku_app37057289');
+  var mongo_db = process.env.MONGO_USER+':'+process.env.MONGO_PW+'@ds031792.mongolab.com:31792/heroku_app37057289';
 }
 
+// Connect
+var db = monk(mongo_db);
+console.log(mongo_db);
+
 // Setup Websockets
-var ws = require('nodejs-websocket');
-var port = 8000;
-var connections = [];
+var ws = require('nodejs-websocket'),
+    port = 8000,
+    connections = [];
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
